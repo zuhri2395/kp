878ae602275aea35fbff16363f38bb21dbb4a679
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2016-08-24 05:58:08 +0700

    rekening

--- input/rekening.php
+++ input/rekening.php
@@ -1,1 +1,47 @@
-input rekening
+<div class="templatemo-content-wrapper">
+	<div class="templatemo-content">
+		<ol class="breadcrumb">
+			<li><a href="index.php">Dashboard</a></li>
+			<li><a href="index.php?posisi=rekening">Rekening</a></li>
+			<li class="active">Input Rekening</li>
+		</ol>
+
+		<h1 class="margin-bottom-15">
+			<b>Rekening</b>
+		</h1>
+		<p class="margin-bottom-15">Form Penambahan Data No Rekening</p>
+		<div class="row">
+			<div class="col-md-12">
+				<form role="form" id="templatemo-preferences-form" action="proses/rekening.php" method="POST">
+					<div class="row">
+						<div class="col-md-12 margin-bottom-15">
+							<label for="nomorRekening" class="control-label">Nomor Rekening</label>
+							<input type="text" name="nomorRekening" class="form-control" placeholder="Nomor Rekening">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12 margin-bottom-15">
+							<label for="judul" class="control-label">Judul</label>
+							<input type="text" name="judul" class="form-control" placeholder="Judul">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12 margin-bottom-15">
+							<label for="uraian" class="control-label">Uraian</label>
+							<textarea class="form-control margin-bottom-15" rows="3" placeholder="Uraian" name="uraian"></textarea>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12 margin-bottom-15">
+							<input type="submit" name="submit" value="Simpan" class="btn btn-primary">
+							<input type="reset" name="reset" value="Reset" class="btn btn-default">
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
--- view/rekening.php
+++ view/rekening.php
@@ -1,1 +1,54 @@
-view rekening
+<div class="templatemo-content-wrapper">
+	<div class="templatemo-content">
+		<ol class="breadcrumb">
+			<li><a href="index.php">Dashboard</a></li>
+			<li class="active">Rekening</li>
+			<li><a href="index.php?posisi=rekening&type=insert">Input Rekening</a></li>
+		</ol>
+
+		<h1 class="margin-bottom-15">
+			<b>Rekening</b>
+		</h1>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="table-responsive">
+					<h4 class="margin-bottom-15">Daftar Rekening</h4>
+					<table class="table table-striped table-hover table-bordered text-center">
+						<thead>
+							<tr>
+								<th class="text-center">No Rekening</th>
+								<th class="text-center">Judul</th>
+								<th class="text-center">Aksi</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+							include_once 'includes/koneksi.php';
+							$sql = "SELECT * FROM rekening";
+							$result = $conn->query($sql);
+							while($row = $result->fetch_object()) {
+								echo "<tr>";
+								echo "<td>" . $row->nomorRekening . "</td>";
+								echo "<td>" . $row->judul . "</td>";
+								echo "<td>";
+								echo "<div class='btn-group'>";
+								echo "<button type='button' class='btn btn-info'>Action</button>";
+								echo "<button type='button' class='btn btn-info dropdown-toggle' data-toggle='dropdown'>";
+								echo "<span class='caret'></span>";
+								echo "<span class='sr-only'>Toggle Dropdown</span>";
+								echo "</button>";
+								echo "<ul class='dropdown-menu' roles='menu'>";
+								echo "<li><a href='index.php?posisi=rekening&type=edit&nomorRekening=" . $row->nomorRekening . "'>Edit</a></li>";
+								echo "<li><a href='index.php?posisi=rekening&type=delete&nomorRekening=" . $row->nomorRekening . "'>Delete</a></li>";
+								echo "</ul>";
+								echo "</tr>";
+							}
+						?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
