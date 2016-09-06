ca14e5e3835f2d99d4821f53c8b2bdbf1f96b029
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2016-09-06 10:53:05 +0700

    proses edit rincian biaya

---
+++ proses/editRBP.php
@@ -0,0 +1,26 @@
+<?php
+include_once '../includes/koneksi.php';
+
+$no = $_POST['no'];
+$noSPPD = $_POST['noSPPD'];
+$hariUangHarian = $_POST['hariUangHarian'];
+$biayaUangHarian = $_POST['biayaUangHarian'];
+$biayaTransport = $_POST['biayaTransport'];
+$biayaPenginapan = $_POST['biayaPenginapan'];
+$hariSewa = $_POST['hariSewa'];
+$biayaSewa = $_POST['biayaSewa'];
+$bendaharaPengeluaran = $_POST['bendaharaPengeluaran'];
+$penerima = $_POST['penerima'];
+$kuasaAnggaran = $_POST['kuasaAnggaran'];
+$pelaksanaKegiatan = $_POST['pelaksanaKegiatan'];
+
+// $sql = "INSERT INTO rincian VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "UPDATE rincian SET noSPPD=?, hariUangHarian=?, biayaUangHarian=?, biayaTransport=?, biayaPenginapan=?, hariSewa=?, biayaSewa=?, bendaharaPengeluaran=?, penerima=?, kuasaAnggaran=?, pelaksanaKegiatan=? WHERE no=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssssssssss", $noSPPD, $hariUangHarian, $biayaUangHarian, $biayaTransport, $biayaPenginapan, $hariSewa, $biayaSewa, $bendaharaPengeluaran, $penerima, $kuasaAnggaran, $pelaksanaKegiatan, $no);
+
+if($stmt->execute()) {
+	header('location:../index.php?posisi=rbp');
+} else {
+	header('location:../index.php?posisi=rbp');
+}
